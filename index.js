04075c17bf6ece50407e248f826c5afd7c0cd51a
Chris Davis <contact@nthstate.com>
2022-11-11 21:10:46 +0000

    chore: Add path requirement

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
+const path = require('path');
 
 function checkFile(src, file, words) {
     try {
