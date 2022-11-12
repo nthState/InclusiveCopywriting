3bd40e0bcfd6c4c2a741c2e7a29625ab2efcaa6b
Chris Davis <contact@nthstate.com>
2022-11-12 12:53:09 +0000

    style: move to two space tabs

--- index.js
+++ index.js
@@ -4,81 +4,81 @@ const fs = require('fs');
 const path = require('path');
 
 const getAllFiles = function(dirPath, arrayOfFiles) {
-    files = fs.readdirSync(dirPath)
+  files = fs.readdirSync(dirPath)
 
-    arrayOfFiles = arrayOfFiles || []
+  arrayOfFiles = arrayOfFiles || []
 
-    files.forEach(function(file) {
-        if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-            arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
-        } else {
-            arrayOfFiles.push(path.join(dirPath, "/", file))
-        }
-    })
+  files.forEach(function(file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(path.join(dirPath, "/", file))
+    }
+  })
 
-    return arrayOfFiles
+  return arrayOfFiles
 }
 
 function checkFile(file, words) {
-    try {
+  try {
 
 
-        core.info(`Reading: ${file}`)
+    core.info(`Reading: ${file}`)
 
-        const data = fs.readFileSync(path.resolve(`${file}`), 'utf8');
+    const data = fs.readFileSync(path.resolve(`${file}`), 'utf8');
 
-        const lines = data.split('\n')
+    const lines = data.split('\n')
 
-        var lineNumber = 1;
-        for (let line of lines) {
-            const tokens = new Set(line.split(' '))
-            let intersection = new Set([...tokens].filter(x => words.has(x)));
-            if (intersection.size > 0) {
+    var lineNumber = 1;
+    for (let line of lines) {
+      const tokens = new Set(line.split(' '))
+      let intersection = new Set([...tokens].filter(x => words.has(x)));
+      if (intersection.size > 0) {
 
-                core.info(`${file} contains ${Array.from(intersection.values())} at line: ${lineNumber}`)
+        core.info(`${file} contains ${Array.from(intersection.values())} at line: ${lineNumber}`)
 
-                return file
-            }
-            lineNumber++;
-        }
-
-
-    } catch (err) {
-        core.info(`Error...${err}`)
+        return file
+      }
+      lineNumber++;
     }
 
-    return undefined
+
+  } catch (err) {
+    core.info(`Error...${err}`)
+  }
+
+  return undefined
 }
 
 async function main() {
-    try {
-        const srcs = (core.getInput('include_paths') || '').split(',').map(t => t.trim());
-        const words = new Set((core.getInput('exclude_words') || '').split(',').map(t => t.trim()));
-        const fileTypeFilter = (core.getInput('file_types') || '').split(',').map(t => t.trim());
-        const warningsAsErrors = core.getBooleanInput('warnings_as_errors')
+  try {
+    const srcs = (core.getInput('include_paths') || '').split(',').map(t => t.trim());
+    const words = new Set((core.getInput('exclude_words') || '').split(',').map(t => t.trim()));
+    const fileTypeFilter = (core.getInput('file_types') || '').split(',').map(t => t.trim());
+    const warningsAsErrors = core.getBooleanInput('warnings_as_errors')
 
-        core.info('Starting...')
-        core.info(`fileTypeFilter: ${fileTypeFilter}`)
+    core.info('Starting...')
+    core.info(`fileTypeFilter: ${fileTypeFilter}`)
 
-        const filesList = srcs.flatMap(path => getAllFiles(path));
+    const filesList = srcs.flatMap(path => getAllFiles(path));
 
-        const filtered = filesList.filter(file => {
-            return fileTypeFilter.some(element => {
-                return file.endsWith(element)
-            });
-        });
+    const filtered = filesList.filter(file => {
+      return fileTypeFilter.some(element => {
+        return file.endsWith(element)
+      });
+    });
 
-        const output = filtered.map(file => checkFile(file, words)).filter(n => n);
+    const output = filtered.map(file => checkFile(file, words)).filter(n => n);
 
-        core.setOutput("files", output);
+    core.setOutput("files", output);
 
-        if (output.length > 0 && warningsAsErrors) {
-            return core.setFailed(`Files contain bad words: ${output}`)
-        }
-
-    } catch (error) {
-        core.setFailed(error.message);
+    if (output.length > 0 && warningsAsErrors) {
+      return core.setFailed(`Files contain bad words: ${output}`)
     }
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 main()
